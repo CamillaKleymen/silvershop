f9dd79d635233920534b703dfb938b18368b80bf
CamillaKleymen <camillakleymen@outlook.com>
2024-06-09 18:47:19 +0500

    aoa

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM python:3.12
 COPY . /silvershop
 WORKDIR /silvershop
 RUN pip install -r requirements.txt
-CMD ["python", "manage.py", "runserver", "--host=0.0.0.0" ,"--port=1706"]
+CMD ["python", "manage.py", "runserver", "--host=0.0.0.0" ,"--port=5631"]
 
--- djproject/settings.py
+++ djproject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'djproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'silvershop',
+        'USER': 'postgres',
+        'PASSWORD': 'muxtar15ovchar',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
